b1dc2beb159117511c2658fc21005a6f2f165b60
Tomasz Klim <github@tomaszklim.pl>
2020-04-20 00:09:04 +0200

    install symlink for pkgconfig

--- internal/common-install.sh
+++ internal/common-install.sh
@@ -16,6 +16,15 @@ echo "/usr/local/opencv-$version/lib" >/etc/ld.so.conf.d/opencv-$version.conf
 ldconfig
 
 
+# remove symlinks, but keep real files (probably installed different way)
+if [ -h /usr/lib/pkgconfig/opencv.pc ]; then
+	rm -f /usr/lib/pkgconfig/opencv.pc
+fi
+if [ ! -f /usr/lib/pkgconfig/opencv.pc ]; then
+	ln -s /usr/local/opencv-$version/lib/pkgconfig/opencv.pc /usr/lib/pkgconfig/opencv.pc
+fi
+
+
 # Python 2.x support
 mkdir -p /usr/local/lib/python2.7/dist-packages
 ln -s /usr/local/opencv-$version/lib/python2.7/dist-packages/cv2.so /usr/local/lib/python2.7/dist-packages
